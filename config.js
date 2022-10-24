d965fbac7ea93941e3af4e31d36b7f427f431b0b
HOUPERT <yoann.houpert@yahoo.fr>
2022-10-24 16:06:20 +0200

    clean config

--- config.js
+++ config.js
@@ -18,17 +18,6 @@ function configureDefaults() {
 
 		process.env.SAAS_API_GATEWAY_HTTP_PORT = ifHas(process.env.SAAS_API_GATEWAY_HTTP_PORT, envdefault.SAAS_API_GATEWAY_HTTP_PORT)
 		process.env.COMPONENTS = ifHas(process.env.COMPONENTS, envdefault.COMPONENTS)
-
-
-
-		process.env.DB_HOST = ifHas(process.env.DB_HOST, envdefault.DB_HOST)
-		process.env.DB_USER = ifHas(process.env.DB_USER, envdefault.DB_USER)
-		process.env.DB_PASS = ifHas(process.env.DB_PASS, envdefault.DB_PASS)
-		process.env.DB_PORT = ifHas(process.env.DB_PORT, envdefault.DB_PORT)
-		process.env.DB_NAME = ifHas(process.env.DB_NAME, envdefault.DB_NAME)
-		process.env.DB_REQUIRE_LOGIN = ifHas(process.env.DB_REQUIRE_LOGIN, envdefault.DB_REQUIRE_LOGIN)
-
-
 	} catch (e) {
 		console.error(e)
 		process.exit(1)
